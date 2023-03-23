7df8d06814577f86a024106836766dd673593df7
risav-sarkar <risavabir2001@gmail.com>
2023-03-23 12:25:28 +0530

    commit2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-console.log("hi");
+const openReplyModal = () => {
+  window.scrollTo(0, 0);
+  document.getElementById("commentModal").style.display = "grid";
+};
+
+const closeReplyModal = () => {
+  document.getElementById("commentModal").style.display = "none";
+};
+
+const submitReplyModal = () => {
+  document.getElementById("commentModal").style.display = "none";
+  alert("Reply send successfully!");
+};
+
+const openEventModal = () => {
+  window.scrollTo(0, 0);
+  document.getElementById("eventModal").style.display = "grid";
+};
+
+const closeEventModal = () => {
+  document.getElementById("eventModal").style.display = "none";
+};
+
+const submitEventModal = () => {
+  document.getElementById("eventModal").style.display = "none";
+  alert("Event created successfully!");
+};
