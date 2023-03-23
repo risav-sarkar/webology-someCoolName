4c902469ac136f73caebf3fa7e9075bd4ca9e726
risav-sarkar <risavabir2001@gmail.com>
2023-03-23 13:08:46 +0530

    commit3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 const openReplyModal = () => {
-  window.scrollTo(0, 0);
   document.getElementById("commentModal").style.display = "grid";
 };
 
@@ -13,7 +12,6 @@ const submitReplyModal = () => {
 };
 
 const openEventModal = () => {
-  window.scrollTo(0, 0);
   document.getElementById("eventModal").style.display = "grid";
 };
 
@@ -24,4 +22,40 @@ const closeEventModal = () => {
 const submitEventModal = () => {
   document.getElementById("eventModal").style.display = "none";
   alert("Event created successfully!");
+};
+
+const selectUpcomingEvents = () => {
+  document.getElementById("upcomingEventTab").classList.add("selected");
+  document.getElementById("pastEventTab").classList.remove("selected");
+};
+
+const selectPastEvents = () => {
+  document.getElementById("pastEventTab").classList.add("selected");
+  document.getElementById("upcomingEventTab").classList.remove("selected");
+};
+
+const selectDailyRevenue = () => {
+  document.getElementById("dailyRevenueTab").classList.add("selected");
+  document.getElementById("monthlyRevenueTab").classList.remove("selected");
+  document.getElementById("yearlyRevenueTab").classList.remove("selected");
+};
+
+const selectMonthlyRevenue = () => {
+  document.getElementById("dailyRevenueTab").classList.remove("selected");
+  document.getElementById("monthlyRevenueTab").classList.add("selected");
+  document.getElementById("yearlyRevenueTab").classList.remove("selected");
+};
+
+const selectYearlyRevenue = () => {
+  document.getElementById("dailyRevenueTab").classList.remove("selected");
+  document.getElementById("monthlyRevenueTab").classList.remove("selected");
+  document.getElementById("yearlyRevenueTab").classList.add("selected");
+};
+
+const openSideBar = () => {
+  document.getElementById("sidebar").style.display = "grid";
+};
+
+const closeSideBar = () => {
+  document.getElementById("sidebar").style.display = "none";
 };
